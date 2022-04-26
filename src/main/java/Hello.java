4b7f69073cc37d7ee7e81b7545adc3f59407f368
lanazayed <lanazayed@gmail.com>
2022-04-26 13:00:15 +0200

    update, added more hello feature.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Github repo is already published to github");
+        System.out.println("doing more work");
     }
 }
